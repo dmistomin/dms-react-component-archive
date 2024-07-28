f20a9bc4a6b482f60776e8db5bf66d37219699ec
Dmitriy Istomin <distomin+git@pm.me>
2024-07-28 13:43:46 -0400

    delete `stories` dir, add example Button component

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,8 +2,7 @@ import type { StorybookConfig } from "@storybook/react-webpack5";
 
 const config: StorybookConfig = {
   stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../src/**/*.stories.tsx"
   ],
   addons: [
     "@storybook/addon-webpack5-compiler-swc",
